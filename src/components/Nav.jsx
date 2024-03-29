f7901219a3f2ffe9c976c1ac114ae54798154f0d
rajugupta0 <raju200115@gmail.com>
2024-03-29 18:18:22 +0530

    Feat: Added categoty filter

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,7 +1,22 @@
+import { useContext } from "react";
+import { ProductContext } from "../utils/Context";
+import { Link } from "react-router-dom";
+
+
 
 const Nav = () => {
-  return (
-    
+
+  const [products] = useContext(ProductContext);
+  
+
+  let uniqueCategory = products && products.reduce((acc,cv) => [...acc,cv.category], [])
+
+  uniqueCategory =  [...new Set(uniqueCategory)];
+  console.log(uniqueCategory);
+
+
+
+  return (  
     <nav  className='w-[15%] h-full bg-zinc-100 flex flex-col items-center pt-5'>
     <a href='/create'
       className='py-2 px-5 border rounded border-blue-200 text-blue-300 '  
@@ -9,25 +24,28 @@ const Nav = () => {
     Add New Product
     </a>
     <hr className='w-[80%] my-3 '></hr>
-
     <h1 className='text-2xl w-[80%] mb-3' >Category</h1>
 
-      <ul className='w-[80%]'>
-        <li>
-          <a href='/'>Cate 1</a>
-        </li>
-        <li>
-          <a href='/about'>Cate 2</a>
-        </li>
-        <li>
-          <a href='/contact'>Cate 3</a>
-        </li>
-      </ul>
+      <div className='w-[80%]'>
+
+      {uniqueCategory.map((c, i) => (
+        <Link
+          key={i}
+          to={`/?category=${c}`}
+          className="flex items-center mb-3"
+        >
+          <span className="rounded-full mr-2 w-[15px] h-[15px] bg-blue-200"></span>{" "}
+          {c}
+        </Link>
+      ))}
+
+       
+      </div>
 
     </nav>
 
     
-  )
-}
+  );
+};
 
 export default Nav
