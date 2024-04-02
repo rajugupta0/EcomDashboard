d54acd4501c91599c20f1f07364ab6dedef688ab
rajugupta0 <raju200115@gmail.com>
2024-04-02 18:28:09 +0530

    ADDED create a edit coponents

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import './App.css'
 import Home from './components/Home'
 import Details from './components/Details'
 import Create from './components/Create';
+import Edit from './components/Edit';
 
 function App() {
    const { search,pathname }= useLocation(); 
@@ -22,6 +23,7 @@ function App() {
     <Routes>
       <Route path='/' element={<Home />} />
       <Route path='/create' element={<Create />} />
+      <Route path='/edit' element={<Edit />} />
       <Route path='/details/:id' element={<Details />} />     
     </Routes>
       
--- src/components/Create.jsx
+++ src/components/Create.jsx
@@ -1,8 +1,31 @@
+import { useState } from "react"
+
 
 
 const Create = () => {
+    const [title, setTitle] = useState("")
+    const [image, setImage] = useState("")
+
   return (
-    <div>Create</div>
+    <>
+    <form className="flex flex-col items-center content-center p-[5%] w-screen h-screen">
+    <h1 className="text-3xl p-2">Add New Product</h1>
+
+    <input 
+        type="url" placeholder="Image Link"  
+        className="text-1.5xl bg-zinc-100 rounded w-1/3 p-3 h-2.5 mb-2"  
+        onChange ={(e) =>  {setImage(e.target.value)}}
+        value={image}    
+    />
+    
+    <input 
+        type="text" placeholder="Title"  
+        className="text-1.5xl bg-zinc-100 rounded w-1/3 p-3 h-2.5  mb-2"  
+        onChange ={(e) =>  {setTitle(e.target.value)}}
+        value={title}    
+    />
+    </form>
+    </>
   )
 }
 
